0f77b5af9e53519280886efb4dd617b00802d78f
yeshaojun <sjye@rsvptech.cn>
2023-01-12 09:19:00 +0800

    34

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
 import TheWelcome from "./components/TheWelcome.vue";
-
-console.log("234");
 </script>
 
 <template>
