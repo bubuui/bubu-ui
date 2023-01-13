ce04a46ba15af1c4e65ecd554b4a1cd32ec0cdc8
yeshaojun <sjye@rsvptech.cn>
2023-01-13 18:00:27 +0800

    feat: add sass

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-import test from "./components/test";
 console.log("12");
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper"><HelloWorld msg="You did it!" /></div>
-  </header>
-
-  <main>
-    <TheWelcome />
-    <test></test>
-  </main>
+  <main>hello word</main>
 </template>
 
 <style scoped>
